47873fa3942deb55dadfe1f47ec29556f9999b5c
Goodkat87 <ilyass.zrl@hotmail.com>
2024-01-22 16:03:12 +0100

    corrections of bugs and amelioration of style for css

--- public/js/main.js
+++ public/js/main.js
@@ -3,17 +3,53 @@ let buttons = document.querySelectorAll("button")
 let result = document.getElementById("result")
 
 let calcule = ""
-
+let lastNumber = false
 Array.from(buttons).forEach(element => {
     element.addEventListener("click",()=>{
         if (element.value === "=") {
-            result.innerText = eval(calcule)
+            //the try instruction verify if there is an error in a code 
+            try{
+                //Function("return"+calcule)() is a function to calcule a string
+                if (calcule == "") {
+                    result.innerText = "0"
+                }else{
+                    result.innerText = Function("return " + calcule)()
+                    calcule = result.innerText
+                    lastNumber = true
+                }
+            }catch(error){
+                result.innerText = "ERROR"
+            }
+            //condition if the user want to put an operator in first charctere
+        }else if (calcule == "" && ["+", "*", "/","."].includes(element.value)) {
+            return
+            // conditon to clear the result 
+        }else if (element.value === "C") {
             calcule = ""
+            result.innerText = "0"
+        //verify if the last input is an operator and disabled the buttons operator
+        }else if (["+", "-", "*", "/","."].includes(calcule.slice(-1)) && ["+", "-", "*", "/","."].includes(element.value)) {
+            return;
+        }else if (lastNumber) {
+            if (["+", "-", "*", "/"].includes(element.value)) {
+                calcule += element.value    
+                result.innerText = calcule
+                lastNumber = false
+            }else if(element.value == "."){
+                calcule = "0."
+                result.innerText=calcule
+                lastNumber = false
+            }else{
+                calcule = ""
+                calcule += element.value
+                result.innerText = calcule
+                lastNumber = false
+            }
         }else{
-            result.innerText = element.value
+            //add value of element in calcule variable
             calcule += element.value
+            result.innerText = calcule
             console.log(calcule);
         }
-
     })
 });
